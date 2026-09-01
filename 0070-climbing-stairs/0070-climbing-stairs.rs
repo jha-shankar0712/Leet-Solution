552bc77954b50f3c3d8dbdc176b299d0c3a5b9e9
Shankar Jha <jhashankar092@gmail.com>
2026-09-01 11:15:13 +0530

    Time: 0 ms (100%), Space: 2.2 MB (47.4%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        if n <= 2 {
+            return n;
+        }
+
+        let mut first = 1;
+        let mut second = 2;
+
+        for _ in 3..=n {
+            let third = first + second;
+            first = second;
+            second = third;
+        }
+
+        second
+    }
+}
