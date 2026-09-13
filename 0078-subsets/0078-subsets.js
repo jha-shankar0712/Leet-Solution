ca8c78823fb7b901dcdbe01d104d966ebcaca643
Shankar Jha <jhashankar092@gmail.com>
2026-09-13 09:30:04 +0530

    Time: 0 ms (100%), Space: 54.7 MB (69.58%) - LeetHub

---
+++ 0078-subsets/0078-subsets.js
@@ -0,0 +1,20 @@
+var subsets = function(nums) {
+    let result = [];
+
+    function backtrack(start, current) {
+        // Har current combination ek valid subset hai
+        result.push([...current]);
+
+        for (let i = start; i < nums.length; i++) {
+            current.push(nums[i]);
+
+            backtrack(i + 1, current);
+
+            current.pop();
+        }
+    }
+
+    backtrack(0, []);
+
+    return result;
+};
