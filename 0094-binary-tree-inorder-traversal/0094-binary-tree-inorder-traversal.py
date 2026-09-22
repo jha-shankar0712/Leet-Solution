183e29cd2a42297c49fe90447620448e362e48e9
Shankar Jha <jhashankar092@gmail.com>
2026-09-22 09:26:28 +0530

    Time: 0 ms (100%), Space: 19.4 MB (28.66%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -0,0 +1,20 @@
+class Solution:
+    def inorderTraversal(self, root):
+        result = []
+
+        def inorder(node):
+            if node is None:
+                return
+
+            # Left
+            inorder(node.left)
+
+            # Root
+            result.append(node.val)
+
+            # Right
+            inorder(node.right)
+
+        inorder(root)
+
+        return result
